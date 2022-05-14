52fd89cc64992c240745e1b50776fcaeb45a8246
gieuagg <69728108+gieuagg@users.noreply.github.com>
2022-05-14 06:28:52 +0300

    Initial commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,56 +1,32 @@
-function calc()
-{
+charsArray = []
 
-let n1 =parseInt(document.getElementById('n1').value);
-let n2 =parseInt(document.getElementById('n2').value);
+charsArray.push(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+charsArray.push("a", "b", "c", "d", "e", "f", "g", "e", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "@", "#", "$", "%", "&", "*", "(", ")", "-", "_", "/", "!", "?", ":")
+console.log(charsArray)
+//same thing happens with the following
+//const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+//const alphabet = alpha.map((x) => String.fromCharCode(x));
+//console.log(alphabet);
 
-let oper = document.getElementById('operators').value;
+function generatePassword(){
+  let passLength = document.getElementById("passLength").value  
+  let customArr = []
+  if(passLength > '7') {
 
-if(oper === '+'){
-	document.getElementById('result').value = n1+n2;
-}
+            
+            for(let i=0; i < 4; i++){  
+            customArr = charsArray[Math.floor(Math.random() * passLength)]
+                
+                         console.log(customArr) 
+                // document.getElementById("suggestion1").textContent = customArr[0]
+                // document.getElementById("suggestion2").textContent = customArr[1]
+                // document.getElementById("suggestion3").textContent = customArr[2]
+                // document.getElementById("suggestion4").textContent = customArr[3]
+                }
+    }
+    else{
+        alert("Please choose a number above 7")
+     }
+    }   
 
-if(oper === '-'){
-	document.getElementById('result').value = n1-n2;
-}
-
-if(oper === '*'){
-	document.getElementById('result').value = n1*n2;
-}
-
-if (oper === '/') {
-	document.getElementById('result').value = n1/n2;
-}
-
-}
-
-
-
-function guessTheNum(){
-	document.getElementById('look').value = Math.floor(Math.random() * 11);
-}
-
-
-
-function changeToUppercase(){
-
-	let lower = document.getElementById('inputString').value;
-	let res = lower.toUpperCase();
-	
-	document.getElementById('inputString').value = res;
-	console.log(res)
-	
-}
-
-
-function changeToLowerCase(){
-	let lower = document.getElementById('inputString').value;
-	let res = lower.toLowerCase();
-	document.getElementById('inputString').value = res;	
-}
-
-function countChars(){
-	let res = document.getElementById('inputString').value;
-	let count = res.length
-	document.getElementById('numRes').value = count;
-}
+generatePassword()
